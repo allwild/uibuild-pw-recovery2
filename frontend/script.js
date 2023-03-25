58971e2cec780c6e8a142a0b843097b21d8522e7
allwild <100026995+allwild@users.noreply.github.com>
2023-03-25 22:19:30 +0100

    project done

---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+document.body.insertAdjacentHTML("afterbegin", `
+  <h1>PASSWORD RECOVERY</h1>
+  <div>
+    Please write the answer of your security question into the following input field and press Enter. </br>Your security question is: </br><span>What was your first pet's name?</span>
+    <input type="text" placeholder="Write your first pet's name here...">
+  </div>
+`)
